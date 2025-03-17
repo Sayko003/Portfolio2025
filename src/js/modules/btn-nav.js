535a230798784d30ae2360fb0d119b1385b43a38
Sayko003 <konstantin.web@bk.ru>
2025-03-18 01:21:06 +0300

    header section is ready

--- src/js/modules/btn-nav.js
+++ src/js/modules/btn-nav.js
@@ -3,7 +3,8 @@ function btnNav(){
     const btnHeader = document.querySelector(`.header .btn--nav`),
         btnMenu = document.querySelector(`.menu .btn--nav`),
         overflow = document.querySelector(`body`),
-        menu = document.querySelector(`.menu`);
+        menu = document.querySelector(`.menu`),
+        overlay = document.querySelector(`.menu .overlay`);
 
     function clickBtn(btn){
         btn.addEventListener(`click`, () =>{
@@ -15,7 +16,7 @@ function btnNav(){
 
     clickBtn(btnHeader);
     clickBtn(btnMenu);
-
+    clickBtn(overlay);
 }
 
 export default btnNav;
