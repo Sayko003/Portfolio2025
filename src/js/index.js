90b9fdc0553336ce2072f6cdd4b82b4ad9c72dff
Sayko003 <konstantin.web@bk.ru>
2025-03-20 21:33:32 +0300

    add id link, script padding for body when open nav

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,9 @@
 import btnNav from "./modules/btn-nav";
 
-btnNav();
+
+document.addEventListener("DOMContentLoaded", function() {
+    btnNav();
+
+
+    console.log(`Привет, если ты разработчик и тебе бы хотелось со мной поработать, на сайте есть ссылочки на контакты со мной. Пиши не стесняйся.`)
+});
--- src/js/modules/btn-nav.js
+++ src/js/modules/btn-nav.js
@@ -4,10 +4,57 @@ function btnNav(){
         btnMenu = document.querySelector(`.menu .btn--nav`),
         overflow = document.querySelector(`body`),
         menu = document.querySelector(`.menu`),
-        overlay = document.querySelector(`.menu .overlay`);
+        overlay = document.querySelector(`.menu .overlay`),
+        linksMenu = document.querySelectorAll(`.menu .menu--link`),
+        bodyPadding = getComputedStyle(overflow);
+
+        
+    //Подсчет ширины скролбара
+    function getScrollBarWidth () {
+        const inner = document.createElement('p');
+        inner.style.width = "100%";
+        inner.style.height = "200px";
+
+        const outer = document.createElement('div');
+        outer.style.position = "absolute";
+        outer.style.top = "0px";
+        outer.style.left = "0px";
+        outer.style.visibility = "hidden";
+        outer.style.width = "200px";
+        outer.style.height = "150px";
+        outer.style.overflow = "hidden";
+        outer.appendChild (inner);
+
+        document.body.appendChild (outer);
+        var w1 = inner.offsetWidth;
+        outer.style.overflow = 'scroll';
+        var w2 = inner.offsetWidth;
+        if (w1 == w2) w2 = outer.clientWidth;
+
+        document.body.removeChild (outer);
+
+        return (w1 - w2);
+    };
+
+    //Проверка есть ли паддинг у body
+    function paddingBody(){
+        const widthScroll = getScrollBarWidth();
+
+        if(bodyPadding.paddingRight === `${widthScroll}px`){
+            overflow.style.paddingRight = `0px`;
+            overflow.style.overflow = `auto`;
+            btnHeader.style.right = `30px`;
+        } else {
+            overflow.style.paddingRight = `${widthScroll}px`;
+            overflow.style.overflow = `hidden`;
+            btnHeader.style.right = `45px`;
+        }
+    }
 
     function clickBtn(btn){
         btn.addEventListener(`click`, () =>{
+        
+        paddingBody();
         overflow.classList.toggle(`no-scroll`);
 
         menu.classList.toggle(`menu-active`);
@@ -17,6 +64,10 @@ function btnNav(){
     clickBtn(btnHeader);
     clickBtn(btnMenu);
     clickBtn(overlay);
+
+    linksMenu.forEach((link, i) =>{
+        clickBtn(link);
+    });
 }
 
 export default btnNav;
